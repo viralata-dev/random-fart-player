d13a88e8b186885c6b9dc5b519f854f4d9c18ea1
Luiza Marques <viralata.dev@gmail.com>
2023-09-22 16:00:28 -0300

    init core features

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const fartList = ["fart-01", "fart-02", "fart-03", "fart-04", "fart-05"];
+
+  const handlePress = () => {
+    const randomFart = Math.floor(Math.random() * fartList.length);
+    alert(fartList[randomFart]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Puxa meu dedo!" onPress={handlePress} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
